713655324f9c6f4f913dd4be229d6d6490948f77
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-01-05 14:42:56 -0500

    add outputs for names of objects, not just IDs

--- output.tf
+++ output.tf
@@ -1,9 +1,21 @@
+output "vnet_name" {
+  value = azurerm_virtual_network.msdn.name
+}
+
 output "vnet_id" {
   value = azurerm_virtual_network.msdn.id
 }
 
+output "subnet_name" {
+  value = azurerm_subnet.msdn1.name
+}
+
 output "subnet_id" {
   value = azurerm_subnet.msdn1.id
+}
+
+output "nsg_name" {
+  value = azurerm_network_security_group.msdn1.name
 }
 
 output "nsg_id" {
